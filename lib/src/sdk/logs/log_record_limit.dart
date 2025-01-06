d7b0ebd5381d53fa0ac9430ed2917fec0ba5c7d7
yusuf <myusufrosman@gmail.com>
2025-01-06 23:42:50 +0800

    wip: script to attach copyright

--- lib/src/sdk/logs/log_record_limit.dart
+++ lib/src/sdk/logs/log_record_limit.dart
@@ -1,3 +1,6 @@
+// Copyright 2021-2022 Workiva.
+// Licensed under the Apache License, Version 2.0. Please see https://github.com/Workiva/opentelemetry-dart/blob/master/LICENSE for more information
+
 class LogRecordLimits {
   static const int defaultMaxAttributeCount = 128;
   static const defaultMaxAttributeLength = -1;
